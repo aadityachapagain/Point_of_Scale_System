c1fff699f36a9cf2e49f398434698979d5227aa0
Aaditya Chapagain <chapagain.aaditya@gmail.com>
2019-01-16 11:50:42 +0545

    added frontend

---
+++ react-pos/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./js/components/Header";
+import Main from "./js/components/Main";
+
+
+const App = (props) => {
+  return(
+    <div>
+      <Main />
+    </div>
+  )
+  };
